f80e6dba4f57bf464eb1df12fb55321bd9fbeafd
Jose Marinho <jose.marinho@arm.com>
2019-11-14 10:39:28 +0000

    arm64: spci/smccc: Create smc call returning on x0 to x7
    The new smc call is SMCCCv1.2 compliant.
    
    Signed-off-by: Jose Marinho <jose.marinho@arm.com>

--- include/linux/arm-smcccv1_2.h
+++ include/linux/arm-smcccv1_2.h
@@ -24,3 +24,14 @@ void __arm_smcccv1_2_hvc(unsigned long a0, unsigned long a1, unsigned long a2,
 			 unsigned long a3, unsigned long a4, unsigned long a5,
 			 unsigned long a6, unsigned long a7,
 			 struct arm_smcccv1_2_return  *res);
+
+/**
+ * __arm_smcccv1_2_smc() - make SMC calls
+ * @a0-a7: arguments passed in registers 0 to 7
+ * @res: result values from registers 0 to 7
+ */
+asmlinkage
+void __arm_smcccv1_2_smc(unsigned long a0, unsigned long a1, unsigned long a2,
+			 unsigned long a3, unsigned long a4, unsigned long a5,
+			 unsigned long a6, unsigned long a7,
+			 struct arm_smcccv1_2_return  *res);
